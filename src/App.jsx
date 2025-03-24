f5e21c9b6e22097d88b4042a2eb1aff95cd9afa2
Ricardo Sequeira <ricarisequeira@gmail.com>
2025-03-24 17:26:07 +0000

    improve responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import Experience from "./components/Experience";
 
 const App = () => {
   const [showButton, setShowButton] = useState(false);
@@ -24,15 +25,16 @@ const App = () => {
   return (
     <div className="absolute inset-0 h-full w-full items-center">
       <Hero />
-      <About />
-      <Projects />
+      <Experience />
       <Skills />
+      <Projects />
+      <About />
       <Contact />
 
       {showButton && (
         <a
           href="#"
-          className="fixed bottom-10 right-10 p-6 bg-orange-500 text-white font-semibold rounded-full shadow-md hover:bg-orange-600"
+          className="fixed bottom-4 sm:bottom-6 md:bottom-8 lg:bottom-10 right-4 sm:right-6 md:right-8 lg:right-10 p-2 sm:p-3 md:p-4 bg-gradient-to-r from-blue-600 to-indigo-400 text-white font-semibold rounded-full shadow-md hover:from-indigo-400 hover:to-blue-600 transition-all duration-300"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +42,7 @@ const App = () => {
             viewBox="0 0 24 24"
             strokeWidth="2"
             stroke="currentColor"
-            className="w-10 h-10"
+            className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6"
           >
             <path
               strokeLinecap="round"
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,36 @@
 import profileImage from "../assets/profile.jpg";
 import GradientButton from "./GradientButton";
+
 const Hero = () => {
   return (
     <section className="min-h-screen flex flex-col justify-center items-center text-center text-white w-full">
       <img
         src={profileImage}
         alt="Profile"
-        className="w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-lg mb-20"
+        className="w-32 h-32 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-lg mb-8 sm:mb-12 md:mb-16 lg:mb-20"
       />
       <h1
-        className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 
-      inline-block text-transparent bg-clip-text mb-6"
+        className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-2 sm:mb-3 md:mb-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 
+      inline-block text-transparent bg-clip-text"
       >
         Ricardo Sequeira
       </h1>
-      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-white-300/80">
+      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 md:mb-8 text-white-300/80">
         Software Engineer
       </p>
 
-      <p className="text-base sm:text-lg md:text-xl lg:text-2xl mt-4 mb-28 max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-3xl mx-auto font-semibold">
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl mt-2 sm:mt-3 md:mt-4 mb-12 sm:mb-16 md:mb-20 lg:mb-28 max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-3xl mx-auto font-semibold">
         With over 5 years of experience in developing scalable cloud-based
         solutions, I am dedicated to delivering high-quality, innovative
         software for impactful projects. Explore my work and see how I can
         contribute to your team's success.
       </p>
 
-      <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4 mt-20">
-        <GradientButton text="About Me" link="#about" />
-        <GradientButton text="Projects" link="#projects" />
+      <div className="flex flex-col md:flex-row justify-center items-center space-y-3 sm:space-y-4 md:space-y-0 md:space-x-4 mt-8 sm:mt-12 md:mt-16 lg:mt-20">
+        <GradientButton text="Experience" link="#experience" />
         <GradientButton text="Skills" link="#skills" />
+        <GradientButton text="Projects" link="#projects" />
+        <GradientButton text="About Me" link="#about" />
         <GradientButton text="Contact" link="#contact" />
       </div>
     </section>
