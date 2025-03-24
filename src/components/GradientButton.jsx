f2e36f387e6bd6d1a897af30fb463c1ab267f4e6
Ricardo Sequeira <ricarisequeira@gmail.com>
2025-03-24 17:35:48 +0000

    improve UI in mobile phones

--- src/components/GradientButton.jsx
+++ src/components/GradientButton.jsx
@@ -1,11 +1,33 @@
 const GradientButton = ({ text, link }) => (
   <a
     href={link}
-    className="relative inline-flex items-center justify-center w-48 overflow-hidden font-semibold text-white rounded-md"
+    className="
+      relative
+      inline-flex
+      items-center
+      justify-center
+      w-48
+      overflow-hidden
+      font-semibold
+      text-white
+      rounded-md
+    "
   >
     <h1
-      className="text-base sm:text-lg md:text-xl lg:text-2xl font-semibold mb-4 bg-gradient-to-r from-blue-600 to-indigo-400
-    inline-block text-transparent bg-clip-text transition-transform duration-300 hover:scale-110 hover:from-indigo-400 hover:to-blue-600"
+      className="
+        text-base sm:text-lg md:text-xl lg:text-2xl
+        font-semibold
+        mb-4
+        bg-gradient-to-r from-blue-600 to-indigo-400
+        inline-block
+        text-transparent
+        bg-clip-text
+        transition-transform
+        duration-300
+        hover:scale-110
+        hover:from-indigo-400
+        hover:to-blue-600
+      "
     >
       {text}
     </h1>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,26 +7,57 @@ const Hero = () => {
       <img
         src={profileImage}
         alt="Profile"
-        className="w-32 h-32 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 rounded-lg mb-8 sm:mb-12 md:mb-16 lg:mb-20"
+        className="
+          w-32 h-32 rounded-lg mb-8
+          sm:w-64 sm:h-64 sm:mb-12
+          md:w-80 md:h-80 md:mb-16
+          lg:w-96 lg:h-96 lg:mb-20
+        "
       />
       <h1
-        className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-2 sm:mb-3 md:mb-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 
-      inline-block text-transparent bg-clip-text"
+        className="
+          text-4xl font-bold mb-2
+          sm:text-6xl sm:mb-3
+          md:text-7xl md:mb-4
+          lg:text-8xl
+          bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 
+          inline-block text-transparent bg-clip-text
+        "
       >
         Ricardo Sequeira
       </h1>
-      <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 md:mb-8 text-white-300/80">
+      <p
+        className="
+        text-xl font-bold mb-4 text-white-300/80
+        sm:text-3xl sm:mb-6
+        md:text-4xl md:mb-8
+        lg:text-5xl
+      "
+      >
         Software Engineer
       </p>
 
-      <p className="text-base sm:text-lg md:text-xl lg:text-2xl mt-2 sm:mt-3 md:mt-4 mb-12 sm:mb-16 md:mb-20 lg:mb-28 max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-3xl mx-auto font-semibold">
+      <p
+        className="
+        text-xs mt-2 mb-8 max-w-xs mx-auto font-semibold
+        sm:text-lg sm:mt-3 sm:mb-16 sm:max-w-xl
+        md:text-xl md:mt-4 md:mb-20 md:max-w-2xl
+        lg:text-2xl lg:mb-28 lg:max-w-3xl
+      "
+      >
         With over 5 years of experience in developing scalable cloud-based
         solutions, I am dedicated to delivering high-quality, innovative
         software for impactful projects. Explore my work and see how I can
         contribute to your team's success.
       </p>
 
-      <div className="flex flex-col md:flex-row justify-center items-center space-y-3 sm:space-y-4 md:space-y-0 md:space-x-4 mt-8 sm:mt-12 md:mt-16 lg:mt-20">
+      <div
+        className="
+        flex flex-col justify-center items-center space-y-1
+        sm:space-y-2
+        md:flex-row md:space-y-0 md:space-x-4 
+      "
+      >
         <GradientButton text="Experience" link="#experience" />
         <GradientButton text="Skills" link="#skills" />
         <GradientButton text="Projects" link="#projects" />
